922e2c77a86845ef1f55806106934555d0457c00
Dave Stanke <davidstanke@google.com>
2025-12-10 12:26:15 -0500

    tweaky tweaky

--- README.md
+++ README.md
@@ -17,7 +17,10 @@ uvicorn agent:a2a_app --port 8001
 
 ## Deploy
 
-Deploy the MCP server
+### Deploy the MCP server
+
+(uses [public] CloudSQL Postgres server described above)
+
 ```bash
 cd mcp-directory-server
 export PROJECT_ID=$(gcloud config get-value project)
@@ -55,4 +58,21 @@ gcloud run deploy springfield-toolbox \
     --args="--tools-file=/app/tools.yaml","--address=0.0.0.0","--port=8080" \
     --allow-unauthenticated \
     # --set-env-vars="PROJECT_ID=$PROJECT_ID,DB_USER=postgres,DB_PASS=admin" \
+```
+
+### Deploy the A2A server
+```
+cd a2a-directory-agent
+export PROJECT_ID=$(gcloud config get-value project)
+gcloud builds submit . --tag=us-central1-docker.pkg.dev/$PROJECT_ID/springfield/a2a-directory-agent
+
+gcloud run deploy a2a-directory-agent \
+  --image=us-central1-docker.pkg.dev/$PROJECT_ID/springfield/a2a-directory-agent:latest \
+  --region=us-central1 \
+  --allow-unauthenticated \
+  --cpu=4 \
+  --memory=2Gi \
+  --network=default \
+  --subnet=default \
+  --set-env-vars=GOOGLE_CLOUD_PROJECT=$PROJECT_ID,GOOGLE_CLOUD_LOCATION=us-central1,GOOGLE_GENAI_USE_VERTEXAI=TRUE,MCP_TOOLBOX_URL=$MCP_TOOLBOX_URL,DJANGO=false,TOOLBOX_URL=https://springfield-toolbox-617191421982.us-central1.run.app,HOST_OVERRIDE=https://a2a-directory-agent-$PROJECT_NUMBER.us-central1.run.app
 ```
--- a2a-directory-agent/agent.py
+++ a2a-directory-agent/agent.py
@@ -4,8 +4,12 @@ import os
 from google.adk.a2a.utils.agent_to_a2a import to_a2a
 from google.adk.agents import Agent
 from toolbox_core import ToolboxSyncClient
+from a2a.types import AgentCard
 
 load_dotenv()
+
+A2A_PORT = os.getenv("A2A_PORT",8001)
+A2A_HOST = os.getenv("HOST_OVERRIDE",f"localhost:{A2A_PORT}")
 
 # TOOLBOX_URL = os.getenv("TOOLBOX_URL", "http://127.0.0.1:5000")
 TOOLBOX_URL = os.getenv("TOOLBOX_URL")
@@ -21,9 +25,40 @@ root_agent = Agent(
     tools=client.load_toolset(),
 )
 
+# # Define A2A agent card
+# my_agent_card = AgentCard(
+#     name="file_agent",
+#     url=A2A_HOST,
+#     description="An agent that can offer information about residents of Springfield",
+#     version="1.0.0",
+#     capabilities={},
+#     skills= [
+#             {
+#             "description": "An agent that can say hello and perform basic tasks. I am a helpful AI assistant designed to provide accurate and useful information about the residents of Springfield.",
+#             "id": "hello_world_agent",
+#             "name": "model",
+#             "tags": [
+#                 "llm"
+#             ]
+#             },
+#             {
+#             "description": "Search residents by name\n\nArgs:\n    first_name (str): The first name of the resident to search for",
+#             "id": "hello_world_agent-search-users",
+#             "name": "search-users",
+#             "tags": [
+#                 "llm",
+#                 "tools"
+#             ]
+#             }
+#         ],
+#     defaultInputModes= ["text/plain"],
+#     defaultOutputModes= ["text/plain"],
+#     supportsAuthenticatedExtendedCard=False,
+# )
+
 # Make the agent A2A-compatible
-A2A_PORT = os.getenv("A2A_PORT",8001)
-a2a_app = to_a2a(root_agent, port=A2A_PORT)
+a2a_app = to_a2a(root_agent, port=A2A_PORT, host=A2A_HOST)
+# a2a_app = to_a2a(root_agent, port=A2A_PORT, agent_card=my_agent_card)
 
 # The to_a2a() function automatically generates the necessary FastAPI/Starlette app
 # and the agent card (.well-known/agent-card.json) in memory.
