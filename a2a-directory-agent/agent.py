5c276ca2eb725a80007e20adcd7dc03c64c17ba2
Dave Stanke <davidstanke@google.com>
2025-12-10 12:36:32 -0500

    remove pycache

--- a2a-directory-agent/agent.py
+++ a2a-directory-agent/agent.py
@@ -19,42 +19,11 @@ client = ToolboxSyncClient(TOOLBOX_URL)
 # Define your agent, including its capabilities/tools
 root_agent = Agent(
     model='gemini-2.5-flash',
-    name='hello_world_agent',
-    description='An agent that can say hello and perform basic tasks.',
-    instruction="You are a helpful AI assistant designed to provide accurate and useful information.",
+    name='springfield_directory',
+    description='An agent that can work with a directory of Springfield residents.',
+    instruction="You are a helpful AI assistant designed to provide accurate and useful information about the residents of Springfield.",
     tools=client.load_toolset(),
 )
-
-# # Define A2A agent card
-# my_agent_card = AgentCard(
-#     name="file_agent",
-#     url=A2A_HOST,
-#     description="An agent that can offer information about residents of Springfield",
-#     version="1.0.0",
-#     capabilities={},
-#     skills= [
-#             {
-#             "description": "An agent that can say hello and perform basic tasks. I am a helpful AI assistant designed to provide accurate and useful information about the residents of Springfield.",
-#             "id": "hello_world_agent",
-#             "name": "model",
-#             "tags": [
-#                 "llm"
-#             ]
-#             },
-#             {
-#             "description": "Search residents by name\n\nArgs:\n    first_name (str): The first name of the resident to search for",
-#             "id": "hello_world_agent-search-users",
-#             "name": "search-users",
-#             "tags": [
-#                 "llm",
-#                 "tools"
-#             ]
-#             }
-#         ],
-#     defaultInputModes= ["text/plain"],
-#     defaultOutputModes= ["text/plain"],
-#     supportsAuthenticatedExtendedCard=False,
-# )
 
 # Make the agent A2A-compatible
 a2a_app = to_a2a(root_agent, port=A2A_PORT, host=A2A_HOST)
